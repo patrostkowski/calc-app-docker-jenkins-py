f63e41a5bb00f3bada747235ca5795a79d648d64
patryk <patryk@debian.patryk>
2020-09-24 15:37:07 +0200

    Added more unit tests

--- src/test_calc.py
+++ src/test_calc.py
@@ -3,18 +3,33 @@
 import pytest
 from calc import add, subtract, multiply, divide, power
 
-def test_add():
+def test_add1():
     assert add(2,2) == 4
 
-def test_subtract():
+def test_add2():
+    assert add(-1,1) == 0
+
+def test_subtract(1):
     assert subtract(2,2) == 0
 
-def test_multiply():
+def test_subtract2():
+    assert subtract(1,-1) == 0
+
+def test_multiply1():
     assert multiply(2,0) == 0
 
-def test_divide():
+def test_multiply2():
+    assert multiply(1,2) == 2
+
+def test_divide1():
     assert divide(2,1) == 2
 
-def test_power():
+def test_divide2():
+    assert divide(3,2) == 2.5
+
+def test_power1():
     assert power(2,2) == 4
+
+def test_power2():
+    assert power(2,3) == 8
 
